0f771066660af56360411dea91a5a6e84e612efc
alvesmwanakunda <thecal4@gmail.com>
2025-04-05 09:23:19 +0000

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,10 @@ export const environment = {
   BASE_API_URL:"https://mlka.app/api",
   BASE_URL:"https://mlka.app"
   //BASE_API_URL:"http://localhost:5000",
+<<<<<<< HEAD
   // BASE_URL:"http://localhost:4200"
+=======
+  //BASE_URL:"http://localhost:4200"
+>>>>>>> 09f389b (env)
 
 };
