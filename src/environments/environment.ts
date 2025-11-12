c87fdd6b39bec65164abdff2668d064cb04a6302
alvesmwanakunda <thecal4@gmail.com>
2025-11-12 01:26:48 +0000

    gestion note

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   BASE_API_URL:"https://mlka.app/api",
   BASE_URL:"https://mlka.app",
-  BASE_SOCKET:"wss://mlka.app:8080",
+  BASE_SOCKET:"wss://mlka.app:8081",
   //BASE_SOCKET:"ws://localhost:8080",
   //BASE_API_URL:"http://localhost:5000",
   //BASE_URL:"http://localhost:4200",
